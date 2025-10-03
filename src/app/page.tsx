256759251203391e5f43776c05d2124a9ffae44c
Aysu Maharramova <125694685+aysumaharramovaa@users.noreply.github.com>
2025-10-03 22:41:35 +0400

    collabworks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -435,6 +435,66 @@ export default function Portfolio() {
           </div>
         </section>
 
+  <section className="max-w-6xl mx-auto px-4 py-10">
+          <h2 className="text-4xl font-bold mb-8 text-center">
+            Collab Works 
+          </h2>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+            {[
+              {
+                src: "/collab1.jpg",
+                alt: "Collab 1",
+                link: "https://www.instagram.com/p/DOnli-mDDMI/?igsh=b2ZzazIxMHdvZ3ls",
+              },
+              {
+                src: "/collab2.jpg",
+                alt: "Collab 2",
+                link: "https://www.instagram.com/p/DOQQTFTDHE-/?igsh=MTBpMGQyeWthd2p2",
+              },
+              {
+                src: "/collab3.jpg",
+                alt: "Collab 3",
+                link: "https://www.instagram.com/p/DLovQrftcJg/?igsh=MWUzdWRvdzZzaWlmbw==",
+              },
+              {
+                src: "/collab4.jpg",
+                alt: "Collab 4",
+                link: "https://www.instagram.com/p/DLEyt5_NnB5/?igsh=c2doY29tajlidnp5",
+              },
+              {
+                src: "/collab5.jpg",
+                alt: "Collab 5",
+                link: "https://www.instagram.com/p/DIoN54BNIDo/?igsh=MWFtMHBnbTZkZzZrMQ==",
+              },
+              {
+                src: "/collab6.jpg",
+                alt: "Collab 6",
+                link: "https://www.instagram.com/p/DIf8ApONxdo/?igsh=MThzY2V0eXA1ZjUxbw==",
+              },
+              {
+                src: "/collab7.jpg",
+                alt: "Collab 7",
+                link: "https://www.instagram.com/p/DJo2AcoI51C/?igsh=MWFkbDVqYjJob3Nveg==",
+              },
+            ].map((item, i) => (
+              <a
+                key={i}
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="relative overflow-hidden rounded-2xl shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-2xl"
+              >
+                <img
+                  src={item.src}
+                  alt={item.alt}
+                  className="w-full h-110 object-cover"
+                />
+              </a>
+            ))}
+          </div>
+        </section>
+
         <section className="max-w-7xl mx-auto px-4 py-10">
           <h2 className="text-4xl font-bold mb-10 text-center">
             25TREND x APAR RIDE
@@ -474,6 +534,7 @@ export default function Portfolio() {
             </p>
           </div>
         </section>
+
 
         <section className="bg-[#113F67] text-[#FDF5AA] py-10 px-4">
           <h2 className="text-4xl font-bold mb-6 text-center">
