66de0a50f9b4afef4794f8717366795b12ae135b
Aysu Maharramova <125694685+aysumaharramovaa@users.noreply.github.com>
2025-12-09 21:29:56 +0400

    psotGridTest2

--- src/app/PostsGrid.tsx
+++ src/app/PostsGrid.tsx
@@ -2,18 +2,12 @@
 
 import { useEffect, useState } from "react";
 
-const images = Array.from({ length: 10 }, (_, i) => `/sebr${i === 0 ? "" : i}.jpg`);
+// 1-ci post
+const sebrImages = Array.from({ length: 10 }, (_, i) => `/sebr${i === 0 ? "" : i}.jpg`);
+// 2-ci post 
+const dusunImages = Array.from({ length: 8 }, (_, i) => `/dusun${i=== 0 ? "" : i }.jpg`);
 
-const Post = () => {
-  const [currentImage, setCurrentImage] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImage((prev) => (prev + 1) % images.length);
-    }, 2000); 
-    return () => clearInterval(interval);
-  }, []);
-
+const Post = ({ images, currentImage }: { images: string[], currentImage: number }) => {
   return (
     <div className="post w-full">
       <img
@@ -26,18 +20,23 @@ const Post = () => {
 };
 
 const PostsGrid = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prev) => prev + 1);
+    }, 3000); 
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="flex flex-wrap gap-4 justify-center">
       <div className="w-full sm:w-1/2 lg:w-1/4">
-        <Post />
+        <Post images={sebrImages} currentImage={currentImage % sebrImages.length} />
       </div>
       <div className="w-full sm:w-1/2 lg:w-1/4">
-        <Post />
+        <Post images={dusunImages} currentImage={currentImage % dusunImages.length} />
       </div>
-      <div className="w-full sm:w-1/2 lg:w-1/4">
-        <Post />
-      </div>
-     
     </div>
   );
 };
